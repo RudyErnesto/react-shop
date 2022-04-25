713c4a74f809cdd67ccd50fab5547c1e53bf4c12
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2022-04-25 19:53:47 -0400

    Arreglando el css de header y home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-
+import Header from '../components/Header';
 const Home = () => {
     return (
-        <div>
-            <h1>React Shop Home</h1>
-        </div>
+        <Header />
     );
 }
 
