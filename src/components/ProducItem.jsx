77466838e442264ee5726420a8496cbe052d67d6
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2022-05-04 16:02:32 -0400

    primer hook

--- src/components/ProducItem.jsx
+++ src/components/ProducItem.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
-import '../styles/ProductItem.scss';
+import React, { useState } from 'react';
+import '@styles/ProductItem.scss';
+import addCar from '@icons/bt_add_to_cart.svg';
 const ProducItem = () => {
+	const [cart, setCart] = useState('');
+	const handleCart = () => {
+		setCart('Hola mundo');
+	}
     return (
         <div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -9,9 +14,10 @@ const ProducItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
-					<img src="./icons/bt_add_to_cart.svg" alt="" />
+				<figure onClick = {handleCart} >
+					<img src={addCar} alt="" />
 				</figure>
+				{cart}
 			</div>
 		</div>
     );
