cb4a190ebc9d9f6d7908c62a8fe628774925efbf
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2022-05-05 22:04:30 -0400

    alt imagenes

--- src/components/ProducItem.jsx
+++ src/components/ProducItem.jsx
@@ -8,10 +8,10 @@ const ProducItem = ({product}) => {
 	}
     return (
         <div className="ProductItem">
-			<img src= {product.images[0]} alt="" />
+			<img src= {product.images[0]} alt={product.title}/>
 			<div className="product-info">
 				<div>
-					<p>${product.price}</p>
+					<p>$ {product.price}</p>
 					<p>{product.title}</p>
 				</div>
 				<figure onClick = {handleCart} >
