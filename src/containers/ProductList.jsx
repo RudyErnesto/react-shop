16f4c6be866f5b3f90082191e71d529d44056ce1
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2022-05-05 21:04:23 -0400

    funcionando el hook

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,27 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import ProducItem from '@components/ProducItem';
-import '../styles/ProductList.scss';
+import useGetProducts from '../hooks/useGetProducts';
+import '@styles/ProductList.scss';
+
 
 
 
 const API ='https://api.escuelajs.co/api/v1/products?limit=5&offset=1';
 
 const ProductList = () => {
-	const [products, setProducts] =  useState([]);
+	const products = useGetProducts(API);
 
-	useEffect(()=>{
-		async function fetchData(){
-			const response = await axios(API);
-			setProducts(response.data);
-		}		
-		fetchData()
-	}, [])
     return (
         <section className="main-container">
 			<div className="ProductList">
 				{products.map(product =>(
-					<ProducItem />
+					<ProducItem product ={product} key = {product.id}  />
 				))}
 			</div>
 		</section>
--- src/hooks/useGetProducts.js
+++ src/hooks/useGetProducts.js
@@ -1,17 +1,16 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const useGetProducts = (API) => {
-    const [products, setProducts]= useState([]);
+    const [products, setProducts] =  useState([]);
 
-    useEffect(async() => {
-		const response = await axios.get(API);
-		setProducts(response.data);
-	}, []);
-
-
-
-
+	useEffect(()=>{
+		async function fetchData(){
+			const response = await axios(API);
+			setProducts(response.data);
+		}		
+		fetchData()
+	}, [])
     return products;
 }
 
