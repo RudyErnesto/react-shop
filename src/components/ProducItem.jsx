0a4814243f4c57e47c0f44139abe6d2a0285d06f
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2022-05-05 21:26:14 -0400

    hook funcionando con la llamada del json

--- src/components/ProducItem.jsx
+++ src/components/ProducItem.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import '@styles/ProductItem.scss';
 import addCar from '@icons/bt_add_to_cart.svg';
-const ProducItem = () => {
+const ProducItem = ({product}) => {
 	const [cart, setCart] = useState([]);
 	const handleCart = () => {
 		setCart([]);
 	}
     return (
         <div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<img src= {product.images[0]} alt="" />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
 				</div>
 				<figure onClick = {handleCart} >
 					<img src={addCar} alt="" />
