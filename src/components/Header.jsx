28e328df299a528c0e39dfbaf4b68cd00feef842
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2022-05-21 18:34:44 -0400

    subiendo boton funcionando de añadir producto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import '@styles/Header.scss';
 import '@components/Menu';
+import AppContext from '@context/AppContext';
 import shoppingCart from '@icons/icon_shopping_cart.svg';
 import menu from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg';
 import Menu from './Menu';
 const Header = () => {
     const [toggle, setToggle] = useState(false);
+    const { state } = useContext(AppContext);
     const handleToggle = () =>{
         setToggle(!toggle); // . ! cambia el estado a su opuesto
     }
@@ -41,7 +43,7 @@ const Header = () => {
                     <li className="navbar-email" onClick = {handleToggle}>email@example.com</li>
                     <li className="navbar-shopping-cart">
                         <img src={shoppingCart} alt="shopping-cart" />
-                        <div>2</div>
+                        {state.cart.length > 0 ? <div > ${state.cart.length}</div> : null } 
                     </li>
                 </ul>
             </div>
