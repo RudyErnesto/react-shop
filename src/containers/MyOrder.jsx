fd36817ebfeb25923010d6c1fbaf99ac7a819295
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2022-05-22 12:32:45 -0400

    terminado para recordar react

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,13 +1,20 @@
 import React, {useContext} from 'react';
-import OrderItem from '@components/Order';
+import OrderItem from '@components/OderItem';
 import AppContext from '@context/AppContext';
 import '@styles/MyOrder.scss';
+import close from '@icons/flechita.svg';
 const MyOrder = () => {
 	const {state} = useContext(AppContext);
+
+	const sumaTotal = () => {
+		const reducer = (accumulator, currentValue) => accumulator+currentValue.price;
+		const sum = state.cart.reduce(reducer, 0);
+		return sum;
+	}
     return (
         <aside className="MyOrder">
 			<div className="title-container">
-				<img src="./icons/flechita.svg" alt="arrow" />
+				<img src={close} alt="arrow" />
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
@@ -18,7 +25,7 @@ const MyOrder = () => {
 					<p>
 						<span>Total</span>
 					</p>
-					<p>$560.00</p>
+					<p>${sumaTotal()}</p>
 				</div>
 				<button className="primary-button">
 					Checkout
